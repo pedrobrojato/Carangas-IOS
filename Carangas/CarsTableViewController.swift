041a56c3146beae544387d54f11039a37e1e2772
Pedro Brojato <pedrobrojato@icloud.com>
2022-03-30 11:48:01 -0300

    Primeiro Commit

---
+++ Carangas/CarsTableViewController.swift
@@ -0,0 +1,114 @@
+//
+//  CarsTableViewController.swift
+//  Carangas
+//
+//  Created by Eric Brito on 21/10/17.
+//  Copyright © 2017 Eric Brito. All rights reserved.
+//
+
+import UIKit
+
+class CarsTableViewController: UITableViewController {
+    var cars: [Car] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        Rest.loadCars(onComplete: { (cars) in
+            self.cars = cars
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }) { (error) in
+            print(error)
+            
+//            switch error {
+//            case.invalidJSON
+//                print("JSON Inválido")
+//            case .noData
+//                print("Sem nenhum JSON")
+//            case .responseStatusCode(code: <#T##Int#>)
+//            }
+        }
+
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "viewSegue" {
+            let vc = segue.destination as! CarViewController
+            vc.car = cars[tableView.indexPathForSelectedRow!.row]
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return cars.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let car = cars[indexPath.row]
+        cell.textLabel?.text = car.name
+        cell.detailTextLabel?.text = car.brand
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            
+            let car = cars[indexPath.row]
+            
+            Rest.delete(car: car, onComplete: { (success) in
+                if success {
+                    
+                    self.cars.remove(at: indexPath.row)
+                    
+                    DispatchQueue.main.async {
+                        tableView.deleteRows(at: [indexPath], with: .fade)
+                    }
+                }
+            })
+        }
+    }
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Carangas/REST.swift
@@ -0,0 +1,124 @@
+//
+//  REST.swift
+//  Carangas
+//
+//  Created by Pedro Brojato on 28/03/22.
+//  Copyright © 2022 Eric Brito. All rights reserved.
+//
+
+import Foundation
+
+enum CarError {
+    case url
+    case taskError(error: Error)
+    case noResponse
+    case noData
+    case responseStatusCode(code: Int)
+    case invalidJSON
+}
+
+enum RestOperation {
+    case save
+    case uptade
+    case delete
+}
+
+class Rest {
+    private static let basePath = "https://carangas.herokuapp.com/cars"
+    
+    private static let configuration: URLSessionConfiguration = {
+        let config = URLSessionConfiguration.default
+        config.allowsCellularAccess = false
+        config.httpAdditionalHeaders = ["Content-Type": "application/json"]
+        config.timeoutIntervalForRequest = 30.0
+        config.httpMaximumConnectionsPerHost = 5
+        return config
+    }()
+    
+    private static let session = URLSession(configuration: configuration)   //URLSession.shared
+    
+    class func loadCars(onComplete: @escaping ([Car]) -> Void, onError: @escaping (CarError) -> Void) {
+        guard let url = URL(string: basePath) else {
+            onError(.url)
+            return
+        }
+        let dataTask = session.dataTask(with: url) {  (data: Data?, response: URLResponse?, error: Error?) in
+            if error == nil {
+                guard let response = response as? HTTPURLResponse else {
+                    onError(.noResponse)
+                    return
+                }
+                if response.statusCode == 200 {
+                    guard let data = data else {return}
+                    do {
+                        let cars = try JSONDecoder().decode([Car].self, from: data)
+                        onComplete(cars)
+                    } catch {
+                        print(error.localizedDescription)
+                        onError(.invalidJSON)
+                    }
+                } else {
+                    print ("Algum status  inválido pelo servidor!!")
+                    onError(.responseStatusCode(code: response.statusCode))
+                }
+            } else {
+                print(error!)
+                onError(.taskError(error: error!))
+            }
+    }
+        dataTask.resume()
+    }
+    
+    class func save(car: Car, onComplete: @escaping (Bool) -> Void) {
+        applyOperation(car: car, operation: .save, onComplete: onComplete)
+    }
+    
+    class func update(car: Car, onComplete: @escaping (Bool) -> Void) {
+        applyOperation(car: car, operation: .uptade, onComplete: onComplete)
+    }
+    
+    class func delete(car: Car, onComplete: @escaping (Bool) -> Void) {
+        applyOperation(car: car, operation: .delete, onComplete: onComplete)
+    }
+
+    private class func applyOperation(car: Car, operation: RestOperation, onComplete: @escaping (Bool) -> Void){
+        
+        let urlString = basePath + "/" + (car._id ?? "")
+        
+        guard let url = URL(string: urlString) else {
+            onComplete(false)
+            return
+        }
+        var httpMethod: String = ""
+        var request = URLRequest(url: url)
+        
+        switch operation {
+            case .save:
+                httpMethod = "POST"
+            case .uptade:
+                httpMethod = "PUT"
+            case .delete:
+                httpMethod = "DELETE"
+        }
+        
+        request.httpMethod = httpMethod
+        guard let json = try? JSONEncoder().encode(car) else {
+            onComplete(false)
+            return
+        }
+        request.httpBody = json
+        
+        let dataTask = session.dataTask(with: request) { (data, response, error) in
+            if error == nil {
+                guard let response = response as? HTTPURLResponse, response.statusCode == 200, let _ = data else {
+                    onComplete(false)
+                    return
+                }
+                onComplete(true)
+            } else {
+                onComplete(false)
+            }
+        }
+        dataTask.resume()
+    }
+}
